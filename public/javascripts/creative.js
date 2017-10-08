972333c031f33c69bec9e83041405c8dd0d4f99a
luca <leonmoka@gmail.com>
2017-10-08 10:21:16 +0200

    first commit

---
+++ public/javascripts/creative.js
@@ -0,0 +1,81 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      console.log('* * * * ' + target);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 48)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 48
+  });
+
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  });
+
+  // Scroll reveal calls
+  window.sr = ScrollReveal();
+  sr.reveal('.sr-icons', {
+    duration: 600,
+    scale: 0.3,
+    distance: '50px',
+    reset:true
+  }, 200);
+  sr.reveal('.btn.btn-primary.btn-xl.js-scroll-trigger', {
+    duration: 600,
+    scale: 0.5,
+    distance: '50px',
+    reset:true
+  }, 200);
+  sr.reveal('.sr-button', {
+    duration: 1000,
+    delay: 200,
+    reset:true
+  });
+  sr.reveal('.sr-contact', {
+    duration: 600,
+    scale: 0.5,
+    distance: '0px',
+    reset:true
+  }, 500);
+
+  // Magnific popup calls
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1]
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+    }
+  });
+
+})(jQuery); // End of use strict
