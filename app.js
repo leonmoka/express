3b89f8341c6df4bfc76f2300bb8c10ba4bec6f3e
luca <leonmoka@gmail.com>
2017-10-08 15:40:40 +0200

    i18n

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -9,6 +10,48 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+
+app.use(cookieParser("express-moka"));
+
+app.use(session({
+    secret: "express-moka",
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}));
+
+
+var i18n = require('i18n');
+
+i18n.configure({
+
+  //define how many languages we would support in our application
+  locales:['en', 'it'],
+
+  //define the path to language json files, default is /locales
+  directory: __dirname + '/locales',
+
+  //define the default language
+  defaultLocale: 'it',
+
+  // define a custom cookie name to parse locale settings from 
+  cookie: 'i18n'
+});
+
+
+app.use(cookieParser("i18n_demo"));
+
+app.use(session({
+    secret: "i18n_demo",
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}));
+
+//init i18n after cookie-parser
+app.use(i18n.init);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -56,6 +99,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.setLocale(req.cookies.i18n);
+  res.render('index', {
+    i18n: res,
+    title: 'Express'
+  });
+});
+
+router.get('/it', function (req, res) {
+  res.cookie('i18n', 'it');
+  res.redirect('/');
+});
+
+router.get('/en', function (req, res) {
+  res.cookie('i18n', 'en');
+  res.redirect('/');
 });
 
 module.exports = router;
